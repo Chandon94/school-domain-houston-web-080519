5cd187c57fba046e2659457dda3313cfdb7e4cd3
Chandon Porter <cneilporter94@gmail.com>
2019-08-10 18:45:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,7 +19,7 @@ class School
    def sort 
      sorted {}
      roster.each do |grade, students|
-       sorted[grade] = students.sorted
+       sorted[grade] = students.sort
      end
      sorted
      
